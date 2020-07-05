ff55c9c6e2066e465789b10d8ec07c2ec5a9f3c0
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-07-05 17:39:42 +0200

    primer contacto con exito con axios

---
+++ public/javascripts/main.js
@@ -0,0 +1,26 @@
+
+
+
+const axiosStandings = axios.create({
+
+    baseURL: 'https:ergast.com/api/f1'
+})
+
+//Standings
+
+
+document.getElementById('form1').onsubmit = e => {
+
+    e.preventDefault()
+
+    const season = document.getElementById('input1').value
+    const driver = document.getElementById('input2').value
+
+    axiosStandings
+        .get(`/${season}/drivers/${driver}/driverStandings.json`)
+        .then(response => {
+        console.log(response.data.MRData.StandingsTable.StandingsLists.DriverStandings[0])
+
+        })
+
+}
