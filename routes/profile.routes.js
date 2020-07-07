d8e4c199e784e7f575f39cb7d5529cd75c9bb803
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-07-07 09:46:10 +0200

    ya pushea ids al array

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -8,12 +8,6 @@ const User = require("../models/user.model")
 // Logged in checker middleware
 const checkAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.redirect('/login')
 
-// Endpoints
-// router.get('/', (req, res) => {
-//     console.log('¿Está el usuario logeado?', req.isAuthenticated())
-//     res.render('index')
-// })
-
 
 // Check logged in session 
 
@@ -21,9 +15,7 @@ router.get('/', checkAuthenticated, (req, res) => {
 
     res.render('profile/profile')
 
-
 })
-
 
 router.get('/:id', checkAuthenticated, (req, res) => {
 
@@ -32,28 +24,17 @@ router.get('/:id', checkAuthenticated, (req, res) => {
     updatedUser.favpilot = req.params.id
 
     User
-
-
         .findOne(req.user)
-
-
         .then(user => {
-            console.log(user)
-            // let arr = user.favpilot
+           
+ 
+            User
+                .findByIdAndUpdate(user._id, { $push: {favpilot: req.params.id} }, {
+                     new: true})
+                .then(() => console.log())
+                .catch(err => console.log(err))
 
-            // arr.push(req.params.id)
-
-
-
-
-            User.
             
-            findByIdAndUpdate(user._id, updatedUser, {
-                    new: true
-                })
-                .then()
-                .catch()
-
 
         })
 
