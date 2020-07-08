7082637f52413409f8e2912aa16c61cb9b1f135c
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-07-08 15:27:54 +0200

    prueba merge

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,9 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Constructor'
     }],
+    // name: { type: String },
+    // path: { type: String },
+    // originalName: { type: String }
 }, {
     timestamps: true
 })
