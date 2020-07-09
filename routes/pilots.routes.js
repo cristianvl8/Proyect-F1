9210d777c78afb9a98177a8f04ffe39a4989f650
cristianvl8 <cristianvinualeslobera@gmail.com>
2020-07-09 19:08:33 +0200

    crud de pilotos con area reservada

--- routes/pilots.routes.js
+++ routes/pilots.routes.js
@@ -1,7 +1,116 @@
 const express = require('express')
 const router = express.Router()
-
+const passport = require("passport")
 const Pilot = require('../models/pilot.model')
+
+
+//Crear Piloto
+
+
+const checkAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.redirect('/login')
+
+router.get('/new', checkAuthenticated, (req, res) => {
+
+    res.render('pilots/pilots-form')
+
+
+})
+
+router.post('/new', checkAuthenticated, (req, res) => {
+
+    const {
+        givenName,
+        familyName,
+        code,
+        nationality,
+        dateOfBirth,
+        permanentNumber
+    } = req.body
+
+    Pilot
+        .create({
+            givenName,
+            familyName,
+            code,
+            nationality,
+            dateOfBirth,
+            permanentNumber,
+            ownerId: req.user.id
+        })
+        .then(() => res.redirect('/pilots'))
+        .catch(err => console.log("Error en la BD", err))
+
+})
+
+
+router.get("/delete/:id", checkAuthenticated, (req, res) => {
+
+    Pilot
+        .findById(req.params.id)
+        .then(thePilot => req.user.id == thePilot.ownerId ?
+
+            Pilot
+            .findByIdAndDelete(req.params.id)
+            .then(() => res.redirect("/pilots"))
+            .catch(err => console.log("Error en la BD", err)) :
+            res.redirect("/login")
+        )
+
+})
+
+
+// Editar Pilotos
+
+router.get("/edit/:id", checkAuthenticated, (req, res) => {
+
+    Pilot
+        .findById(req.params.id)
+        .then(thePilot => 
+
+            req.user.id == thePilot.ownerId ?
+                res.render('pilots/pilots-edit-form', thePilot) :
+                res.redirect("/login"))
+
+})
+
+
+router.post("/edit/:id", checkAuthenticated, (req, res) => {
+
+    const {
+        givenName,
+        familyName,
+        code,
+        nationality,
+        dateOfBirth,
+        permanentNumber,
+        
+
+    } = req.body
+
+    Pilot
+
+        .findById(req.params.id)
+        .then(thePilot => req.user.id == thePilot.ownerId ?
+
+            Pilot
+            .findByIdAndUpdate(req.params.id, {
+                givenName,
+                familyName,
+                code,
+                nationality,
+                dateOfBirth,
+                permanentNumber,
+                ownerId: req.user.id
+            })
+            .then(() => res.redirect("/pilots")):
+            res.redirect("/login"))
+
+
+})
+
+
+
+//Listar Pilotos
 
 router.get('/', (req, res) => {
 
@@ -9,14 +118,19 @@ router.get('/', (req, res) => {
         .find()
         .then(allPilots => {
 
-            res.render("pilots/pilots-list", { allPilots })
+            res.render("pilots/pilots-list", {
+                allPilots
+            })
 
         })
 
 })
 
+
+// Detalles de Pilotos
+
 router.get('/:id', (req, res) => {
-    
+
 
     Pilot
         .findById(req.params.id)
@@ -25,7 +139,6 @@ router.get('/:id', (req, res) => {
         })
 
 })
-
 
 
 
